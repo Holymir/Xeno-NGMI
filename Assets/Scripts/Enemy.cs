849fc48ceb267fb626435ce5846507b908d46fae
holymir <ventsi@limechain.tech>
2022-07-15 22:11:37 +0300

    initialized
    basic mechanics

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    [HideInInspector]
+    public Transform player;
+
+    public float stopDistance;
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            if (Vector2.Distance(transform.position, player.position) > stopDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public Enemy enemy;
+        public int count;
+        public float timeBetweenSpawns;
+    }
+
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+    public float timeBetweenWaves;
+
+    private Wave currentWave;
+    private int currentWaveIndex;
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(StartNextWave(currentWaveIndex));
+
+    }
+
+    IEnumerator StartNextWave(int index)
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+        StartCoroutine(SpawnWave(index));
+    }
+
+    IEnumerator SpawnWave(int index)
+    {
+        currentWave = waves[index];
+
+        for (int i = 0; i < currentWave.count; i++)
+        {
+            if (player == null)
+            {
+                yield break;
+            }
+            Enemy randomEnemy = currentWave.enemy;
+            Transform randomSpot = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomSpot.position, randomSpot.rotation);
+            yield return new WaitForSeconds(currentWave.timeBetweenSpawns);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
